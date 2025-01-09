3c7dcaf6978d08dcb0c6819ebfabfa6c327460ef
Michel Lopes Cruz <lopesmichel489@gmail.com>
2025-01-09 15:45:12 +0000

    arquivo para escrever as consultas sql

---
+++ query.sql
@@ -0,0 +1,13 @@
+INSERT INTO "Funcionarios" ("matricula", "nome")  VALUES
+    ('30246307', 'Luziane Cutrim Gomes'),
+	('30246257', 'Elane Roberto Da Silva Lima'),
+	('30246348', 'Jhonathan Yan Silva Maciel'),
+	('30246351', 'Mateus Martins Pontes'),
+	('30246360', 'Mario Sergio Da Silva'),
+	('30246346', 'Valdean Sousa Moreira'),
+	('30246352', 'Cesar Augusto Da Silva'),
+	('30246380', 'Rodenilson De Freitas Carvalho'),
+	('30246350', 'Cleilson Dos Santos Spindola'),
+	('30246280', 'Naildo Lopes Da Silva'),
+	('30246029', 'Felipe Angelin Vieira De Souza'),
+	('30246332', 'Huan Jefferson Costa Gomes');
